15c7f37ad670b53ad104597684d74793081dd8ac
Mirii <44065639+Miriiz@users.noreply.github.com>
2022-05-11 14:34:34 +0200

    Add files via upload

---
+++ .github/workflows/src/docGeneration/main.py
@@ -0,0 +1,6 @@
+from src.utils.function import *
+
+if __name__ == '__main__':
+    code = getFunctFromFile(os.path.dirname(__file__))
+    codeArray = createCodeAndSummarize(code)
+    # print(codeArray)
