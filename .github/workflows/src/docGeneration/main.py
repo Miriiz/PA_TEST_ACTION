8659f882b5b9c4cf2bf8da1fda9b6746bfba3167
Mirii <marvinmiri@hotmail.fr>
2022-05-13 00:30:44 +0200

    fichier requirement au reparage

--- .github/workflows/src/docGeneration/main.py
+++ .github/workflows/src/docGeneration/main.py
@@ -5,7 +5,7 @@ from utils.function import *
 if __name__ == '__main__':
     print("path")
     print(os.path.abspath(__file__))
-    code = getFunctFromFile(os.path.dirname(__file__))
+    code = getFunctFromFile(os.path.dirname(os.path.abspath(__file__)))
 
     codeArray = createCodeAndSummarize(code)
     print(codeArray)
