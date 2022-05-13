99635d97945d1a06306b317ed3112f7d2765cd23
Mirii <marvinmiri@hotmail.fr>
2022-05-13 10:12:33 +0200

    idefix

--- .github/workflows/src/docGeneration/main.py
+++ .github/workflows/src/docGeneration/main.py
@@ -7,4 +7,11 @@ if __name__ == '__main__':
     code = getFunctFromFile(os.path.dirname(os.path.abspath(__file__)))
 
     codeArray = createCodeAndSummarize(code)
-    print(codeArray)
+    pdf = PDF()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font('Times', '', 12)
+    for func in codeArray:
+        pdf.cell(0, 10, func[0], 0, 1)
+        pdf.cell(0, 10, func[1][0]['summary_text'], 0, 1)
+    pdf.save("test")
