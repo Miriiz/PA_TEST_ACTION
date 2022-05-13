d0d4eb98e25781c78de3bba32b8196758fe268f2
Mirii <marvinmiri@hotmail.fr>
2022-05-13 11:18:38 +0200

    test

--- .github/workflows/src/docGeneration/main.py
+++ .github/workflows/src/docGeneration/main.py
@@ -14,5 +14,5 @@ if __name__ == '__main__':
     for func in codeArray:
         pdf.add_function(func)
     pdf.save(os.path.dirname(os.path.abspath(__file__))+'/output', "test")
-    print(os.listdir(os.path.dirname(os.path.abspath(__file__)) + '/output'))
+    print(os.listdir(os.path.dirname(__file__) + '/output'))
 
