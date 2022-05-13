df3c61e9f1d84337877af30382751ad62a3382e0
Mirii <marvinmiri@hotmail.fr>
2022-05-13 18:32:08 +0200

    fix  something

--- .github/workflows/src/docGeneration/main.py
+++ .github/workflows/src/docGeneration/main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     pdf.set_font('Times', '', 12)
     for func in codeArray:
         pdf.add_function(func)
-    pdf_path = ''.join(os.path.dirname(os.path.abspath(__file__)).split(IGNORE_DIR_NAME)[0]) + '/output/'
+    pdf_path = ''.join(os.path.dirname(os.path.abspath(__file__)).split(IGNORE_DIR_NAME)[0]) + 'output/'
     print(pdf_path)
     pdf.save(pdf_path, "test")
     print(os.listdir(os.path.dirname(__file__) + '/output'))
