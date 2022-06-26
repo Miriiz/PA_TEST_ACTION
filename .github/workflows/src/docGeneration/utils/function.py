7f228cf13565262633c92382ed900ad8e5c44f99
Mirii <marvinmiri@hotmail.fr>
2022-06-27 00:21:22 +0200

    update path

--- .github/workflows/src/docGeneration/utils/function.py
+++ .github/workflows/src/docGeneration/utils/function.py
@@ -233,5 +233,5 @@ def write_cosine(functionName, consineValue):
         with open(path + 'commentarySimilarity.log', 'w+') as f:
             f.write("Commentary Similarity for function :  " + functionName + ' : \t' + str(consineValue) + '% \n')
             print("Commentary Similarity for function :  " + functionName + ' : \t' + str(consineValue) + '% \n')
-            print(path, 'commentarySimilarity.log')
+            print(path+'commentarySimilarity.log')
             os.system('git add ' + path + 'commentarySimilarity.log')
