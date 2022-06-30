f650c26857c100aa2fc8e853ad4a811a8ea7a98b
Mirii <marvinmiri@hotmail.fr>
2022-06-30 15:09:13 +0200

    Test pa

--- .github/workflows/src/docGeneration/main.py
+++ .github/workflows/src/docGeneration/main.py
@@ -1,7 +1,7 @@
-import os.path
-
 from utils.function import *
-from PdfCreator import PDF
+from utils.PdfCreator import PDF
+from utils.ModelFunctions import *
+from myconfig import *
 
 if __name__ == '__main__':
     code, files_paths = getFunctFromFile(os.path.dirname(os.path.abspath(__file__)))
@@ -9,15 +9,25 @@ if __name__ == '__main__':
     function_index = 0
     pdf = PDF()
     pdf.alias_nb_pages()
-    #pdf.add_page()
-    # pdf.add_summary(None)
+    pdf.startPageNums()
+    # pdf.add_page()
+
+    y = np.zeros(len(code))
+    test = prepare_dataset(code, y)
+    model = create_base_model(add_mlp_layers2, encoder)
+    model.load_weights('model\\mlp2')
+
+    valu, label = next(iter(test))
+    predicts = model.predict(valu)
 
     for file_path in files_paths:
         pdf.add_path(file_path[0])
         for i in range(function_index, function_index + file_path[1]):
-            pdf.add_error("error")
+            if predicts[function_index] > 0.5:
+                pdf.add_error("Possible memory error in this function")
             pdf.add_function(codeArray[function_index])
             function_index += 1
 
+    pdf.insertTOC()
     pdf_path = ''.join(os.path.dirname(os.path.abspath(__file__)).split(IGNORE_DIR_NAME)[0]) + 'output/'
-    pdf.save(pdf_path, "test")
+    pdf.save(pdf_path, pdf_name)
